34e4d5f4addbad31ff1f01a894668978e12b5a36
Ham <hyoungchan@unist.ac.kr>
2019-07-30 10:45:52 +0900

    recover_page

--- secondproject/settings.py
+++ secondproject/settings.py
@@ -57,8 +57,8 @@ ROOT_URLCONF = 'secondproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        # 'DIRS': ['secondproject/templates'],
+        # 'DIRS': [],
+        'DIRS': ['secondproject/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
