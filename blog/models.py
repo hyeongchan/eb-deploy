e34bebaed95266dbd30f503a0a6fc4b424ed1b08
Ham <hyoungchan@unist.ac.kr>
2019-07-30 11:01:29 +0900

    edit model

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Blog(models.Model):
+    objects = models.Manager()
     title = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
     body = models.TextField()
@@ -11,4 +12,4 @@ class Blog(models.Model):
         return self.title
 
     def summary(self):
-        return self.body[:30]
+        return self.body[:100]
